356f865f762c12cfed141f5ca8c1c6c9178a5fe0
Brandon Marella <brandonmarella@gmail.com>
2026-04-02 23:35:55 -0400

    Added disabled state to buttons

--- aurelia-test/src/components/styled-button/styled-button.ts
+++ aurelia-test/src/components/styled-button/styled-button.ts
@@ -5,16 +5,22 @@ export class StyledButton {
   @bindable fill: boolean = false;
   @bindable disabled: boolean = false;
 
-  @bindable action: () => void = (): void => { console.log("HI - " + this.variant); }
+  @bindable action: () => void;
+
+  handleClick() {
+    this.action?.();
+  }
 
   get buttonClasses() {
     return {
-      'px-4 py-2 text-sm font-bold rounded-sm flex items-center justify-center gap-2 transition-all cursor-pointer': true,
+      'px-4 py-2 text-sm font-bold rounded-sm flex items-center justify-center gap-2 transition-all': true,
       'text-white bg-sky-600 hover:bg-sky-500': this.variant === 'primary',
       'text-slate-600 bg-slate-200 hover:bg-slate-100': this.variant === 'secondary',
       'text-green-500 bg-green-800 hover:bg-green-700': this.variant === 'accept',
       'text-red-800 bg-red-200 hover:bg-red-100': this.variant === 'destructive',
-      'w-fit': !this.fill
+      'w-fit': !this.fill,
+      'cursor-pointer': !this.disabled,
+      'opacity-40': this.disabled
     }
   }
 }
